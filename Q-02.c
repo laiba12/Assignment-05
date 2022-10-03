34025734cc2ba85bd67cd160c682680f630fe195
laiba12 <laiba.noor312@gmail.com>
2022-10-03 18:34:57 +0530

    Add files via upload

---
+++ Q-02.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a=1,n;
+    printf("Enter the number= ");
+    scanf("%d",&n);
+    while(a<=n)
+    {
+        printf("%d\n",a);
+    a++;
+    }
+    return 0;
+
+}
